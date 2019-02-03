5329ddb0ce2553fc1c5ee1d6a4bc262517ef068c
Richard Gao <rgao@olin.edu>
2019-02-02 19:39:21 -0500

    Added correct camera ratio and aim line

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Alert, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Alert, Image, Platform, Dimensions } from 'react-native';
 import { Camera, Permissions } from 'expo';
 
 export default class CameraScreen extends React.Component {
@@ -16,37 +16,87 @@ export default class CameraScreen extends React.Component {
   async getResistorValue() {
     if (this.camera) {
       let photo = await this.camera.takePictureAsync();
+      console.log(`[PICTURE] Width: ${photo.width} Height: ${photo.height}`); // width: 3456, height: 4608
       this.setState({
         image: photo.uri
       });
+
+      // Process images
+
+    }
+  }
+
+  // Get ratio of devices camera so display is not distored
+  async getRatio() {
+    if (Platform.OS === 'android' && this.camera) {
+      const ratios = await this.camera.getSupportedRatiosAsync();
+
+      const { height, width } = Dimensions.get('window');
+      const phoneRatio = height / width;
+      var bestRatio = 0;
+      var threshold = 100000;
+
+      // Get best ratio out of those frim getSupportedRatios
+      for (ratio in ratios) {
+          const r = ratios[ratio].split(":")
+          if (Math.abs(phoneRatio - r[0] / r[1]) < threshold) {
+              threshold = Math.abs(bestRatio - r[0]/r[1]);
+              bestRatio = ratios[ratio];
+          }
+      }
+
+      this.setState({ ratio: bestRatio });
     }
   }
 
   render() {
-    const { hasCameraPermission } = this.state;
+    const { hasCameraPermission, ratio, image } = this.state;
     if (hasCameraPermission === null) {
       return <View />;
     } else if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
     } else {
+      let width = height = null;
+      if (ratio) {
+        width = Dimensions.get('window').width;
+        const r = ratio.split(":");
+        height = width * r[0] / r[1];
+        console.log(`[CAMERA] Width: ${width} Height: ${height}`);
+      }
+      
+      // TODO: Display line up resister here text
       return (
         <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} zoom={1} focusDepth={1} ref={ref => { this.camera = ref; }}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                alignItems: 'center',
-                justifyContent: 'flex-end',
-              }}>
-              { this.state.image ? <Image style={{width: 193, height: 110}} source={{uri: this.state.image}}/> : null }
+          <Camera 
+            style={{ width: width, height: height, }}
+            zoom={1}
+            focusDepth={1}
+            ref={ref => { this.camera = ref; }}
+            onCameraReady={this.getRatio.bind(this)}
+            ratio={ratio}
+          >
+            <View style={[
+              styles.aimLine, 
+              {
+                width: 100,
+                marginLeft: width / 2 - 100 / 2
+              }
+            ]}></View>
+
+            <View style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+            }}>
+              { image ? <Image style={{ width: 200, height: 200}} source={{uri: image}}/> : null }
               <TouchableHighlight 
                 onPress={this.getResistorValue.bind(this)} 
                 underlayColor="white" 
                 style={styles.button}
               >
                 <View>
-                  <Text style={styles.buttonText}>Capture</Text>
+                  <Text>Capture</Text>
                 </View>
               </TouchableHighlight>
             </View>
@@ -69,5 +119,10 @@ const styles = StyleSheet.create({
     height: 50,
     alignItems: 'center',
     backgroundColor: 'white',
+  },
+  aimLine: {
+    flex: 1,
+    borderBottomWidth: 3,
+    borderBottomColor: 'red',
   }
 });
